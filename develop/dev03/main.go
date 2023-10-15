6d6cc24f0494789b678a0a9942d7f98347cbc7a1
revanite <evgkol69@gmail.com>
2023-10-15 16:16:46 -0400

    Dev03: added sorting algorithm, need to figure out comparison

--- develop/dev03/main.go
+++ develop/dev03/main.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
-type flags struct {
+type options struct {
 	k uint
 	n bool
 	r bool
@@ -18,19 +19,35 @@ type flags struct {
 	h bool
 }
 
+var flags options
+
 func main() {
-	flags := flags{}
 	argv := os.Args
 	filename, err := parseArgs(argv, &flags)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	fmt.Println(flags)
-	fmt.Println(filename)
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	lines := strings.Split(string(bytes), "\n")
+	if flags.c {
+		sorted := checkIfSorted(lines)
+		if sorted {
+			fmt.Println("Lines in file '" + filename + "' are sorted")
+		} else {
+			fmt.Println("Lines in file '" + filename + "' are not sorted")
+			os.Exit(1)
+		}
+	} else {
+		quicksort(lines, 0, len(lines)-1)
+	}
 }
 
-func parseArgs(argv []string, flags *flags) (string, error) {
+func parseArgs(argv []string, flags *options) (string, error) {
 	var filename string
 	i := 1
 	for i < len(argv) {
@@ -78,4 +95,54 @@ func parseArgs(argv []string, flags *flags) (string, error) {
 		}
 	}
 	return filename, nil
+}
+
+func quicksort(lines []string, low, high int) {
+	if low >= high {
+		return
+	}
+	pivot := quicksortPartition(lines, low, high)
+	quicksort(lines, low, pivot-1)
+	quicksort(lines, pivot+1, high)
+}
+
+func quicksortPartition(lines []string, low, high int) int {
+	pivot := lines[high]
+	i := low - 1
+	for j := low; j < high; j++ {
+		if compare(lines[j], pivot) {
+			i++
+			lines[i], lines[j] = lines[j], lines[i]
+		}
+	}
+	lines[i+1], lines[high] = lines[high], lines[i+1]
+	return i + 1
+}
+
+func checkIfSorted(lines []string) bool {
+	for i := 1; i < len(lines); i++ {
+		if !compare(lines[i-1], lines[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func compare(left, right string) bool {
+	var result bool
+	if flags.k != 0 {
+
+	} else if flags.n {
+
+	} else if flags.M {
+
+	} else if flags.h {
+
+	} else {
+
+	}
+	if flags.r {
+		result = !result
+	}
+	return result
 }
